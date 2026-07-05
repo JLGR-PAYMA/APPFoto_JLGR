d461b26d271b17d9cfaef9ad9a862d63ddfe8651
JLGR-PAYMA <jlgrprevencion@gmail.com>
2026-07-05 20:56:48 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,16 @@
+{
+  "name": "Cámara PWA GPS y Voz",
+  "short_name": "CamGPS+Voz",
+  "start_url": "index.html",
+  "display": "standalone",
+  "background_color": "#f0f0f0",
+  "theme_color": "#2196F3",
+  "orientation": "portrait",
+  "icons": [
+    {
+      "src": "https://flaticon.com",
+      "sizes": "512x512",
+      "type": "image/png"
+    }
+  ]
+}
